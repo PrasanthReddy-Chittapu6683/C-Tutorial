a4500591914385b12cd6339217f5674954fe6574
prcv <prcv@cisco.com>
2020-11-12 05:04:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+
+
+    public abstract class myAb
+    {
+        public abstract string printString(string name);
+       public  int EmpId;
+        public void EmpDtls()
+        {
+            Console.WriteLine("Inside Ab");
+        }
+    }
+
+    
+    class Program : myAb
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Singleton fromEmp = new Singleton();
+            //fromEmp.printSingletonMessages("This is from Emp ");
+
+            //Singleton fromStud = new Singleton();
+            //fromStud.printSingletonMessages("This is from Student");
+
+            myAb m = new Program();
+            m.EmpDtls();
+            m.printString("Prcv");
+            m.EmpId = 10;
+
+
+        }
+        public override string printString(string name)
+        {
+            Console.WriteLine("My Name:" + name);
+            return name;
+        }
+
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Singleton
+    {
+        private static int counter = 0;
+        public Singleton()
+        {
+            counter++;
+            Console.WriteLine("This is Counter in Singleton Class Constructor : " + counter);
+        }
+        public void printSingletonMessages(string msg)
+        {
+            Console.WriteLine(msg);
+
+        }
+    }
+}
